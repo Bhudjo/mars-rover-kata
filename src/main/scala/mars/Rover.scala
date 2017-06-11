3ae2816e1421664eaa1fd2485a584b526181a6cf
Alessandro Buggin <alessandrobuggin@gmail.com>
2017-06-11 18:14:09 +0000

    * master: green bar; minimal code added

---
+++ src/main/scala/mars/Rover.scala
@@ -0,0 +1,11 @@
+package mars
+
+/**
+  * Created by abuggin on 11/06/17.
+  */
+
+object Rover {
+  val defaultPosition: Any = ""
+
+  def receivePosition(EmptyPosition: Any) = defaultPosition
+}
--- src/test/scala/me/buggin/mars/PositionSpec.scala
+++ src/test/scala/me/buggin/mars/PositionSpec.scala
@@ -1,11 +1,14 @@
 package me.buggin.mars
 
+import mars.Rover
 import org.scalatest._
 
 /**
   * Created by abuggin on 11/06/17.
   */
 class PositionSpec extends WordSpec {
+
+  val EmptyPosition: Any = ""
 
   "A Rover" when {
     "receives an empty instruction array" should {
